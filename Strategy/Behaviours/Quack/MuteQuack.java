e332d0bd9708630d9eb4694f115afdbd3aa8cc40
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-10-26 13:31:00 +0300

    Added QuackBehaviour concreate classes

---
+++ Strategy/Behaviours/Quack/MuteQuack.java
@@ -0,0 +1,10 @@
+package Behaviours.Quack;
+
+public class MuteQuack implements QuackBehaviour {
+
+    @Override
+    public void quack() {
+        System.out.println("<< Silence >>");
+    }
+
+}
---
+++ Strategy/Behaviours/Quack/Quack.java
@@ -0,0 +1,10 @@
+package Behaviours.Quack;
+
+public class Quack implements QuackBehaviour {
+
+    @Override
+    public void quack() {
+        System.out.println("Quack");
+    }
+
+}
