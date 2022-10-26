9974c84140ec6f9a23649d0ef49d7ece2fb34cbb
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-10-26 13:30:32 +0300

    Added FlyBehaviour concreate classes

---
+++ Strategy/Behaviours/Fly/FlyNoWay.java
@@ -0,0 +1,10 @@
+package Behaviours.Fly;
+
+public class FlyNoWay implements FlyBehaviour {
+
+    @Override
+    public void fly() {
+        System.out.println("I can't fly");
+    }
+
+}
---
+++ Strategy/Behaviours/Fly/FlyWithWings.java
@@ -0,0 +1,10 @@
+package Behaviours.Fly;
+
+public class FlyWithWings implements FlyBehaviour {
+
+    @Override
+    public void fly() {
+        System.out.println("I'm flying!");
+    }
+
+}
