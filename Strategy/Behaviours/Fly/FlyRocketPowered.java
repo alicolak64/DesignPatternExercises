59f201bebdb01541c280d81136a6c375205fb4e6
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-10-26 13:33:04 +0300

    Added new FlyBehaviour

---
+++ Strategy/Behaviours/Fly/FlyRocketPowered.java
@@ -0,0 +1,10 @@
+package Behaviours.Fly;
+
+public class FlyRocketPowered implements FlyBehaviour {
+
+    @Override
+    public void fly() {
+        System.out.println("I'm flying with a rocket");
+    }
+
+}
