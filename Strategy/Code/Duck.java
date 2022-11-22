d5a05ce4307003a2ff72e1673b9992bb53805a20
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-11-22 13:11:49 +0300

    Added Duck abstract class

---
+++ Strategy/Code/Duck.java
@@ -0,0 +1,46 @@
+package DesignPatterns.StrategyDesignPattern.Code;
+
+import DesignPatterns.StrategyDesignPattern.Code.Behaviours.Fly.FlyBehaviour;
+import DesignPatterns.StrategyDesignPattern.Code.Behaviours.Quack.QuackBehaviour;
+
+public abstract class Duck {
+
+    FlyBehaviour flyBehaviour;
+    QuackBehaviour quackBehaviour;
+
+    public Duck () {
+
+    }
+
+    public void performFly() {
+        flyBehaviour.fly();
+    }
+
+    public void performQuack() {
+        quackBehaviour.quack();
+    }
+
+    public void swim() {
+        System.out.println("All ducks float, even decoys!");
+    }
+
+    public abstract void display();
+
+    public void setFlyBehaviour(FlyBehaviour fb) {
+        flyBehaviour = fb;
+    }
+
+    public void setQuackBehaviour(QuackBehaviour qb) {
+        quackBehaviour = qb;
+    }
+
+    public void getFlyBehaviour() {
+        flyBehaviour.fly();
+    }
+
+    public void getQuackBehaviour() {
+        quackBehaviour.quack();
+    }
+
+
+}
