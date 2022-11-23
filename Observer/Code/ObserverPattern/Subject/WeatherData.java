296ba895c951e9d6df65064745f303a14126d418
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-11-23 14:45:12 +0300

    Added Subject Concreate Class

---
+++ Observer/Code/ObserverPattern/Subject/WeatherData.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class WeatherData implements Subject{
+    private float temperature;
+    private float humidity;
+    private float pressure;
+    private ArrayList<Observer> observers;
+
+    public WeatherData() {
+        observers = new ArrayList<>();
+    }
+
+    @Override
+    public void registerObserver(Observer o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        int i = observers.indexOf(o);
+        if (i >= 0) {
+            observers.remove(i);
+        }
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update(temperature, humidity, pressure);
+        }
+    }
+    public void measurementsChanged() {
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+}
