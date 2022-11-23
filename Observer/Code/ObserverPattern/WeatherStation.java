4da05917d3f4c8113cd25fb3b268b5fd7f2c6ca5
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-11-23 14:49:23 +0300

    Added client code

--- Observer/Code/ObserverPattern/WeatherStation.java
+++ Observer/Code/ObserverPattern/WeatherStation.java
@@ -7,8 +7,7 @@ public class WeatherStation {
         CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
         StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
         ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
-        ThirdPartyDisplay thirdPartyDisplay = new ThirdPartyDisplay(weatherData);
-        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
+        
 
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
