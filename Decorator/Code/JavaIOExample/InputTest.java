508eb6a2b172c2c2e77fe0eecfd614e57c8e6b14
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-11-24 09:37:41 +0300

    Add client code for java.io

---
+++ Decorator/Code/JavaIOExample/InputTest.java
@@ -0,0 +1,20 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputTest {
+
+    public static void main(String[] args) {
+        int c;
+        try {
+            InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("test.txt")));
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
