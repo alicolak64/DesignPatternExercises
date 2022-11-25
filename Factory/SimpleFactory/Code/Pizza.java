21df7d0003fd193216181c68f2e550c3ebba8d23
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-11-25 09:56:47 +0300

    Added Pizza Super Class

---
+++ Factory/SimpleFactory/Code/Pizza.java
@@ -0,0 +1,33 @@
+package DesignPatterns.FactoryDesignPattern.SimpleFactory.Code;
+
+public class Pizza {
+
+    private String name;
+
+    public void prepare() {
+        System.out.println("Preparing " + name);
+    }
+
+    public void bake() {
+        System.out.println("Baking " + name);
+    }
+
+    public void cut() {
+        System.out.println("Cutting " + name);
+    }
+
+    public void box() {
+        System.out.println("Boxing " + name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
