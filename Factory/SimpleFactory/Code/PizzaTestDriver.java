7238e2b8a922eac752d5ecf1862f38fdf54c88dd
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-11-25 09:58:08 +0300

    Added Client Class

---
+++ Factory/SimpleFactory/Code/PizzaTestDriver.java
@@ -0,0 +1,18 @@
+package DesignPatterns.FactoryDesignPattern.SimpleFactory.Code;
+
+public class PizzaTestDriver {
+
+    public static void main(String[] args) {
+
+        SimplePizzaFactory factory = new SimplePizzaFactory();
+
+        PizzaStore pizzaStore = new PizzaStore(factory);
+
+        Pizza pizza = pizzaStore.orderPizza("cheese");
+        System.out.println("We ordered a " + pizza.getName() + " pizza");
+
+
+
+    }
+
+}
