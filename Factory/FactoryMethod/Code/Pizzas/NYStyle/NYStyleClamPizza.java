c4c850afd48142e6346d35832999ac9633104bcd
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-11-29 12:51:19 +0300

    Added nystyle concreate pizza classes

---
+++ Factory/FactoryMethod/Code/Pizzas/NYStyle/NYStyleClamPizza.java
@@ -0,0 +1,12 @@
+public class NYStyleClamPizza extends Pizza {
+
+    public NYStyleClamPizza() {
+        name = "NY Style Sauce and Clam Pizza";
+        dough = "Thin Crust Dough";
+        sauce = "Marinara Sauce";
+
+        toppings.add("Grated Reggiano Cheese");
+        toppings.add("Fresh Clams from Long Island Sound");
+    }
+
+}
