71b4f5200c90e5b2ccf5b9db028394fbab9416b5
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-11-29 12:49:11 +0300

    Deleted package name and imports

--- Factory/SimpleFactory/Code/Pizzas/PepperoniPizza.java
+++ Factory/SimpleFactory/Code/Pizzas/PepperoniPizza.java
@@ -1,7 +1,3 @@
-package DesignPatterns.FactoryDesignPattern.SimpleFactory.Code.Pizzas;
-
-import DesignPatterns.FactoryDesignPattern.SimpleFactory.Code.Pizza;
-
 public class PepperoniPizza extends Pizza {
 
     public PepperoniPizza() {
