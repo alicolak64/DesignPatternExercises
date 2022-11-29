35de600713ad7afd7aec4758f79c587c6bb37a16
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-11-29 12:51:41 +0300

    Added chicago concreate pizza classes

---
+++ Factory/FactoryMethod/Code/Pizzas/ChicagoStyle/ChicagoStyleVeggiePizza.java
@@ -0,0 +1,18 @@
+public class ChicagoStyleVeggiePizza extends Pizza {
+
+    public ChicagoStyleVeggiePizza() {
+        name = "Chicago Style Veggie Pizza";
+        dough = "Extra Thick Crust Dough";
+        sauce = "Plum Tomato Sauce";
+
+        toppings.add("Shredded Mozzarella Cheese");
+        toppings.add("Black Olives");
+        toppings.add("Spinach");
+        toppings.add("Eggplant");
+    }
+
+    protected void cut() {
+        System.out.println("Cutting the pizza into square slices");
+    }
+
+}
