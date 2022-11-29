7f8c086fb8036e04ef7f5944e342213b008a842f
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-11-30 01:17:25 +0300

    Added pizza ingredient factory concreate classes

---
+++ Factory/AbstractFactory/Code/IngredientFactories/NYPizzaIngredientFactory.java
@@ -0,0 +1,34 @@
+public class NYPizzaIngredientFactory implements PizzaIngredientFactory {
+
+    @Override
+    public Dough createDough() {
+        return new ThinCrustDough();
+    }
+
+    @Override
+    public Sauce createSauce() {
+        return new MarinaraSauce();
+    }
+
+    @Override
+    public Cheese createCheese() {
+        return new ReggianoCheese();
+    }
+
+    @Override
+    public Veggies[] createVeggies() {
+        Veggies veggies[] = { new Garlic(), new Onion(), new Mushroom(), new RedPepper() };
+        return veggies;
+    }
+
+    @Override
+    public Pepperoni createPepperoni() {
+        return new SlicedPepperoni();
+    }
+
+    @Override
+    public Clams createClam() {
+        return new FreshClams();
+    }
+
+}
