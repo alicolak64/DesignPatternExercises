4e448c04ecfdf473e29abcd46d57d8eb38a76144
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-11-29 12:50:59 +0300

    Added pizzastore concreate classes (factory)

---
+++ Factory/FactoryMethod/Code/PizzaStores/ChicagoPizzaStore.java
@@ -0,0 +1,22 @@
+public class ChicagoPizzaStore extends PizzaStore {
+
+
+    @Override
+    protected Pizza createPizza(String type) {
+
+        if (type.equals("cheese")) {
+            return new ChicagoStyleCheesePizza();
+        } else if (type.equals("veggie")) {
+            return new ChicagoStyleVeggiePizza();
+        } else if (type.equals("clam")) {
+            return new ChicagoStyleClamPizza();
+        } else if (type.equals("pepperoni")) {
+            return new ChicagoStylePepperoniPizza();
+        } else return null;
+
+    }
+
+    public String getName() {
+        return "ChicagoPizzaStore";
+    }
+}
---
+++ Factory/FactoryMethod/Code/PizzaStores/NYPizzaStore.java
@@ -0,0 +1,22 @@
+public class NYPizzaStore extends PizzaStore {
+
+    @Override
+    protected Pizza createPizza(String type) {
+
+        if (type.equals("cheese")) {
+            return new NYStyleCheesePizza();
+        } else if (type.equals("veggie")) {
+            return new NYStyleVeggiePizza();
+        } else if (type.equals("clam")) {
+            return new NYStyleClamPizza();
+        } else if (type.equals("pepperoni")) {
+            return new NYStylePepperoniPizza();
+        } else
+            return null;
+    }
+
+    public String getName() {
+        return "NYPizzaStore";
+    }
+
+}
