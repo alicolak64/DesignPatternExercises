c9a9c64b609295c7cc22ab2263c9c2e69ff267c3
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-12-05 03:46:24 +0300

    Add client class

--- Command/Code/SingleCommand/RemoteControlTest.java
+++ Command/Code/SingleCommand/RemoteControlTest.java
@@ -3,8 +3,8 @@ public class RemoteControlTest {
     public static void main(String[] args) {
 
         SimpleRemoteControl remote = new SimpleRemoteControl();
-        Light light = new Light();
-        LightOnCommand lightOn = new LightOnCommand(light);
+        SimpleLight light = new SimpleLight();
+        SimpleLightOnCommand lightOn = new SimpleLightOnCommand(light);
 
         remote.setCommand(lightOn);
         remote.buttonWasPressed();
