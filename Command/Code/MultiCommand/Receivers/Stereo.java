388cafd0d2d80d202134d92159af6ea4e5cec78e
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-12-06 00:06:07 +0300

    Added new features to stereo receiver

--- Command/Code/MultiCommand/Receivers/Stereo.java
+++ Command/Code/MultiCommand/Receivers/Stereo.java
@@ -1,33 +1,54 @@
 public class Stereo {
 
     String location = "";
+    int volume = 0;
 
     public Stereo(String location) {
         this.location = location;
     }
 
     public void on() {
-        System.out.println("Stereo is on");
+        System.out.println( location + " stereo is on");
     }
 
     public void off() {
-        System.out.println("Stereo is off");
+        System.out.println( location + " stereo is off");
     }
 
     public void setCD() {
-        System.out.println("Stereo is set for CD input");
+        System.out.println( location + " stereo is set for CD input");
     }
 
     public void setDVD() {
-        System.out.println("Stereo is set for DVD input");
+        System.out.println( location + " stereo is set for DVD input");
     }
 
     public void setRadio() {
-        System.out.println("Stereo is set for Radio");
+        System.out.println( location + " stereo is set for Radio");
     }
 
     public void setVolume(int volume) {
-        System.out.println("Stereo volume set to " + volume);
+        if (volume <= 30 && volume >= 0) {
+            this.volume = volume;
+            System.out.println( location + " stereo volume set to " + volume);
+        }
+    }
+
+    public void volumeUp() {
+        if (volume < 30) {
+            volume++;
+            System.out.println( location + " stereo volume up to " + volume);
+        } else
+            System.out.println( location + " stereo volume maximum level");
+
+    }
+
+    public void volumeDown() {
+        if (volume > 0) {
+            volume--;
+            System.out.println( location + " stereo volume down to " + volume);
+        } else
+            System.out.println( location + " stereo volume minimum level");
     }
 
 }
