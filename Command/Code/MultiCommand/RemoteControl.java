da63b1405400e557e63c74fb5c749a1ba1462b8c
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-12-05 03:49:08 +0300

    Added invoker class  for multi command

---
+++ Command/Code/MultiCommand/RemoteControl.java
@@ -0,0 +1,40 @@
+public class RemoteControl {
+
+    SimpleCommand[] onCommands;
+    SimpleCommand [] offCommands;
+
+    public RemoteControl() {
+        onCommands = new SimpleCommand[7];
+        offCommands = new SimpleCommand[7];
+
+        SimpleCommand noCommand = new NoCommand();
+        for (int i = 0; i < 7; i++) {
+            onCommands[i] = noCommand;
+            offCommands[i] = noCommand;
+        }
+    }
+
+    public void setCommand (int slot, SimpleCommand onCommand, SimpleCommand offCommand) {
+        onCommands[slot] = onCommand;
+        offCommands[slot] = offCommand;
+    }
+
+    public void onButtonWasPushed(int slot) {
+        onCommands[slot].execute();
+    }
+
+    public void offButtonWasPushed(int slot) {
+        offCommands[slot].execute();
+    }
+
+    public String toString() {
+        StringBuilder stringBuff = new StringBuilder();
+        stringBuff.append("\n------ Remote Control -------\n");
+        for (int i = 0; i < onCommands.length; i++)
+            stringBuff.append("[slot ").append(i).append("] ").append(onCommands[i].getClass().getName()).append("    ").append(offCommands[i].getClass().getName()).append("\n");
+
+        return stringBuff.toString();
+
+    }
+
+}
