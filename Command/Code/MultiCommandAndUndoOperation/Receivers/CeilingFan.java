26a23ce2fcfcb94f450ae0eeb7dcf17df377097f
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-12-06 00:45:13 +0300

    Added receiver classes

---
+++ Command/Code/MultiCommandAndUndoOperation/Receivers/CeilingFan.java
@@ -0,0 +1,44 @@
+public class CeilingFan {
+    String location = "";
+    int speed;
+
+    public CeilingFan(String location) {
+        this.location = location;
+    }
+
+    public void high() {
+        speed = 3;
+        System.out.println(location + " ceiling fan is on high");
+    }
+
+    public void medium() {
+        speed = 2;
+        System.out.println(location + " ceiling fan is on medium");
+    }
+
+    public void low() {
+        speed = 1;
+        System.out.println(location + " ceiling fan is on low");
+    }
+
+    public void off() {
+        speed = 0;
+        System.out.println(location + " ceiling fan is off");
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void on() {
+        if (speed == 0)
+            low();
+        else if (speed == 1)
+            medium();
+        else if (speed == 2)
+            high();
+        else if (speed == 3)
+            low();
+    }
+
+}
