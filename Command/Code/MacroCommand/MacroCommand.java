44c59625d5c0a400c921b87159158ce5d65263e9
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-12-06 01:46:43 +0300

    Added Macro Command class

---
+++ Command/Code/MacroCommand/MacroCommand.java
@@ -0,0 +1,16 @@
+public class MacroCommand implements Command {
+
+    Command[] commands;
+
+    public MacroCommand(Command[] commands) {
+        this.commands = commands;
+    }
+
+    @Override
+    public void execute() {
+        for (Command command : commands) {
+            command.execute();
+        }
+    }
+
+}
