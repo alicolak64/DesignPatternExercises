87b9355c8a56b51a8b7549054f4d4d04b499ed0d
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-12-06 01:44:44 +0300

    Added receiver class

---
+++ Command/Code/MacroCommand/Receivers/Light.java
@@ -0,0 +1,15 @@
+public class Light {
+    String location = "";
+
+    public Light(String location) {
+        this.location = location;
+    }
+
+    public void on() {
+        System.out.println(location + " light is on");
+    }
+
+    public void off() {
+        System.out.println(location + " light is off");
+    }
+}
---
+++ Command/Code/MacroCommand/Receivers/Stereo.java
@@ -0,0 +1,54 @@
+public class Stereo {
+
+    String location = "";
+    int volume = 0;
+
+    public Stereo(String location) {
+        this.location = location;
+    }
+
+    public void on() {
+        System.out.println( location + " stereo is on");
+    }
+
+    public void off() {
+        System.out.println( location + " stereo is off");
+    }
+
+    public void setCD() {
+        System.out.println( location + " stereo is set for CD input");
+    }
+
+    public void setDVD() {
+        System.out.println( location + " stereo is set for DVD input");
+    }
+
+    public void setRadio() {
+        System.out.println( location + " stereo is set for Radio");
+    }
+
+    public void setVolume(int volume) {
+        if (volume <= 30 && volume >= 0) {
+            this.volume = volume;
+            System.out.println( location + " stereo volume set to " + volume);
+        }
+    }
+
+    public void volumeUp() {
+        if (volume < 30) {
+            volume++;
+            System.out.println( location + " stereo volume up to " + volume);
+        } else
+            System.out.println( location + " stereo volume maximum level");
+
+    }
+
+    public void volumeDown() {
+        if (volume > 0) {
+            volume--;
+            System.out.println( location + " stereo volume down to " + volume);
+        } else
+            System.out.println( location + " stereo volume minimum level");
+    }
+
+}
