dbe571af967ab8ee85f3270f595a46f61ae03a08
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-12-05 14:42:47 +0300

    Added command concreate class

---
+++ Command/Code/SingleCommand/LightOnCommand.java
@@ -0,0 +1,12 @@
+public class LightOnCommand implements Command {
+    Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.on();
+    }
+
+}
