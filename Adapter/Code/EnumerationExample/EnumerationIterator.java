de8d6d19a2258a3ba78c67e9403b86d64a1ad6db
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-12-12 00:35:03 +0300

    Add Adapter Class From Enumeration to Iterator

---
+++ Adapter/Code/EnumerationExample/EnumerationIterator.java
@@ -0,0 +1,21 @@
+public class EnumerationIterator implements Iterator {
+
+    private Enumeration enumeration;
+
+    public EnumerationIterator(Enumeration enumeration) {
+        this.enumeration = enumeration;
+    }
+
+    public boolean hasNext() {
+        return enumeration.hasMoreElements();
+    }
+
+    public Object next() {
+        return enumeration.nextElement();
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+
+}
