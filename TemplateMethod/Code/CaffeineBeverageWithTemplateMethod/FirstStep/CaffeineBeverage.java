d5e2477665372de211b8d11a3474a4b8be795f6f
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-12-12 19:26:22 +0300

    Added Bevarage Super Class To First Step

---
+++ TemplateMethod/Code/CaffeineBeverageWithTemplateMethod/FirstStep/CaffeineBeverage.java
@@ -0,0 +1,12 @@
+public abstract class CaffeineBeverage {
+
+    abstract void prepareRecipe();
+    void boilWater() {
+        System.out.println("Boiling water");
+    }
+
+    void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+
+}
