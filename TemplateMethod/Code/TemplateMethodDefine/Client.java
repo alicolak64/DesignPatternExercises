7a4e1dfb286c6da64a079fe44151d1119d9d366a
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-12-12 20:28:15 +0300

    Add client class

---
+++ TemplateMethod/Code/TemplateMethodDefine/Client.java
@@ -0,0 +1,15 @@
+package DesignPatterns.TemplateMehodPattern.Code.TemplateMethodDefine;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        AbstractClass class1 = new ConcreteClass1();
+        AbstractClass class2 = new ConcreteClass2();
+
+        class1.templateMethod();
+        class2.templateMethod();
+
+    }
+
+}
