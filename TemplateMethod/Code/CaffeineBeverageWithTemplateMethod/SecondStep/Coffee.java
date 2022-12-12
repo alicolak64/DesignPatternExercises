692001cd49b8092b57c067e2bf2083faa304e4b1
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-12-12 19:27:46 +0300

    Added Bevarage Concreate Classeses For Second Step

---
+++ TemplateMethod/Code/CaffeineBeverageWithTemplateMethod/SecondStep/Coffee.java
@@ -0,0 +1,13 @@
+public class Coffee extends CaffeineBeverage {
+
+    @Override
+    void brew() {
+        System.out.println("Dripping Coffee through filter");
+    }
+
+    @Override
+    void addCondiments() {
+        System.out.println("Adding Sugar and Milk");
+    }
+
+}
