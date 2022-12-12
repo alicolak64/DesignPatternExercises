d011e412b3c2a404e8230837c72bd4c0e5a6db14
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-12-12 19:25:24 +0300

    Added Inıtal Codes Starbuzz Coffee

---
+++ TemplateMethod/Code/CaffeineBeverageInitialCode/Tea.java
@@ -0,0 +1,22 @@
+public class Tea {
+
+    void prepareRecipe() {
+        boilWater();
+        steepTeaBag();
+        pourInCup();
+        addLemon();
+    }
+    public void boilWater() {
+        System.out.println("Boiling water");
+    }
+    public void steepTeaBag() {
+        System.out.println("Steeping the tea");
+    }
+    public void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+    public void addLemon() {
+        System.out.println("Adding Lemon");
+    }
+
+}
