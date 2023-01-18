36cdedb3641ef0182fa306a46a95631dc8644ca2
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2023-01-18 19:38:14 +0300

    Added concreate menu classes

---
+++ Iterator/Code/MyIterator/Menus/PancakeHouseMenu.java
@@ -0,0 +1,30 @@
+public class PancakeHouseMenu implements Menu {
+    static final int MAX_ITEMS = 6;
+    int numberOfItems = 0;
+    MenuItem[] menuItems;
+
+    public PancakeHouseMenu() {
+        menuItems = new MenuItem[MAX_ITEMS];
+
+        addItem("K&B's Pancake Breakfast", "Pancakes with scrambled eggs, and toast", true, 2.99);
+        addItem("Regular Pancake Breakfast", "Pancakes with fried eggs, sausage", false, 2.99);
+        addItem("Blueberry Pancakes", "Pancakes made with fresh blueberries", true, 3.49);
+        addItem("Waffles", "Waffles, with your choice of blueberries or strawberries", true, 3.59);
+    }
+
+    public void addItem(String name, String description, boolean vegetarian, double price) {
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        if (numberOfItems >= MAX_ITEMS) {
+            System.err.println("Sorry, menu is full!  Can't add item to menu");
+        } else {
+            menuItems[numberOfItems] = menuItem;
+            numberOfItems = numberOfItems + 1;
+        }
+    }
+
+    public Iterator createIterator() {
+        return new PancakeHouseMenuIterator(menuItems);
+    }
+
+    // other menu methods here
+}
