61555e0b6af0348f2f3863e41c957672558b5720
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2023-01-18 19:38:27 +0300

    Added waitress class

---
+++ Iterator/Code/MyIterator/Waitress.java
@@ -0,0 +1,30 @@
+public class Waitress {
+    Menu pancakeHouseMenu;
+    Menu dinerMenu;
+
+    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu) {
+        this.pancakeHouseMenu = pancakeHouseMenu;
+        this.dinerMenu = dinerMenu;
+    }
+
+    public void printMenu() {
+        Iterator pancakeIterator = pancakeHouseMenu.createIterator();
+        Iterator dinerIterator = dinerMenu.createIterator();
+        System.out.println("MENU \t\t BREAKFAST");
+        printMenu(pancakeIterator);
+        System.out.println("MENU \t\t LUNCH");
+        printMenu(dinerIterator);
+    }
+
+    private void printMenu(Iterator iterator) {
+        while (iterator.hasNext()) {
+            MenuItem menuItem = (MenuItem)iterator.next();
+            System.out.print(menuItem.getName() + ", ");
+            System.out.print(menuItem.getPrice() + " -- ");
+            System.out.println(menuItem.getDescription());
+        }
+    }
+
+    // other menu methods here
+
+}
