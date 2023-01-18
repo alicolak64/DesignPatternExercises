65bc1cb991c774092ccdbad0d04b54cec55256db
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2023-01-18 19:40:25 +0300

    Added client code

---
+++ Iterator/Code/JavaIterator/MenuTestDrive.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class MenuTestDrive {
+
+    public static void main(String[] args) {
+
+
+        PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+        DinerMenu dinerMenu = new DinerMenu();
+        CafeMenu cafeMenu = new CafeMenu();
+        Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu, cafeMenu);
+        waitress.printMenu();
+
+
+    }
+
+}
