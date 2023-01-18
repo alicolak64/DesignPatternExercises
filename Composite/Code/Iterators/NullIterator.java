ccb4b407ad6e912b185886d64896a40843277f37
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2023-01-18 19:50:01 +0300

    Added null object NullIterator concreate iterator

---
+++ Composite/Code/Iterators/NullIterator.java
@@ -0,0 +1,17 @@
+import java.util.Iterator;
+
+public class NullIterator implements Iterator<MenuComponent> {
+
+    public MenuComponent next() {
+        return null;
+    }
+
+    public boolean hasNext() {
+        return false;
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+
+}
